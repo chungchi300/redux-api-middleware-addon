d23229fa1b396b0beb1a309a457cf03ec0d3db1d
Astri Jeff Chung <jeffchung@astri.org>
2017-12-19 10:02:00 +0800

    fix

--- make/app/dev.js
+++ make/app/dev.js
@@ -5,8 +5,7 @@ import globals from 'rollup-plugin-node-globals';
 import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
 import includePaths from 'rollup-plugin-includepaths';
-import json from 'rollup-plugin-json';
-//future
+
 let includePathOptions = {
   paths: ['src/common'],
 };
@@ -16,25 +15,13 @@ export default {
   format: 'iife',
   plugins: [
     includePaths(includePathOptions),
-
     babel({
       babelrc: true,
       exclude: 'node_modules/**',
       runtimeHelpers: true,
     }),
-
     cjs({
       sourceMap: false,
-
-      namedExports: {
-        'node_modules/react/react.js': [
-          'Children',
-          'Component',
-          'PropTypes',
-          'createElement',
-        ],
-        'node_modules/react-dom/index.js': ['render'],
-      },
     }),
     globals(),
     replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
