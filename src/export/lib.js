4caafcf6f0d6626e142d50207c0faac9bb7b4a8a
Astri Jeff Chung <jeffchung@astri.org>
2017-12-19 11:29:04 +0800

    fix

--- src/export/lib.js
+++ src/export/lib.js
@@ -1,6 +1,6 @@
 import * as Action from 'actions';
 import * as Helper from 'helpers/api';
-import * as Reducer from './reducers';
+import Reducer from './reducers';
 export default {
   Action,
   Helper,
