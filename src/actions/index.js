f4e31ef819266bf27174f41585eb1e4bbac24f8d
Jeff Chung <chungchi300@hotmail.com>
2018-03-16 15:22:10 +0800

    refactor

---
+++ src/actions/index.js
@@ -0,0 +1,29 @@
+import URI from 'urijs';
+import { CALL_API } from 'redux-api-middleware';
+import { stringify } from 'query-string';
+import _ from 'lodash';
+import { subsituteUrl, formData, processType } from 'helpers/api';
+export function setProtocol(protocol) {
+  return {
+    type: 'API:SET_PROTOCOL',
+    payload: protocol,
+  };
+}
+
+export function setSwagger(swagger) {
+  return {
+    type: 'API:SET_SWAGGER',
+    payload: swagger,
+  };
+}
+export function setHeaders(headers) {
+  return {
+    type: 'API:SET_BASE_HEADERS',
+    payload: {
+      headers: headers,
+    },
+  };
+}
+export async function request(pathName, { method, data, subst }, types) {
+  await 'yolo';
+}
