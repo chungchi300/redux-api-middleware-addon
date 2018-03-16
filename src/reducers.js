056a364390dbb5e686ec8ca81c03ab1620173fe5
Jeff Chung <chungchi300@hotmail.com>
2018-03-16 15:37:59 +0800

    fix

--- src/reducers.js
+++
@@ -1,9 +0,0 @@
-import api from 'reducers/api';
-import domain from 'reducers/domain';
-import network from 'reducers/network';
-
-export default {
-  domain,
-  network,
-  api,
-};
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import api from './api';
+import network from './network';
+import domain from './domain';
+
+export default {
+  domain,
+  api,
+  network,
+};
