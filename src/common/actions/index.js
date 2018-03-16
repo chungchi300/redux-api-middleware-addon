482e1b8832e5940741c1702ba94153c2fd3a814f
Jeff Chung <chungchi300@hotmail.com>
2018-03-16 16:37:31 +0800

    fix

---
+++ src/common/actions/index.js
@@ -0,0 +1,69 @@
+import URI from 'urijs';
+import { CALL_API } from 'redux-api-middleware';
+import { stringify } from 'query-string';
+import _ from 'lodash';
+import { subsituteUrl, formData, processType } from 'helpers/api';
+export function setProtocol(protocol) {
+  return {
+    type: 'API:SET_PROTOCOL',
+    payload: protocol,
+  };
+}
+
+export function setSwagger(swagger) {
+  return {
+    type: 'API:SET_SWAGGER',
+    payload: swagger,
+  };
+}
+export function setHeaders(headers) {
+  return {
+    type: 'API:SET_BASE_HEADERS',
+    payload: {
+      headers: headers,
+    },
+  };
+}
+export function request(pathName, { method, data, subst }, types) {
+  const pathEntity = _.get(getState().api.paths, pathName);
+
+  let entityPath = pathName;
+  if (subst) {
+    entityPath = subsituteUrl(pathName, subst);
+  }
+  let realPath = entityPath;
+
+  // //TODO Header should be create by Header constructor instead of plain object
+  let headers = {
+    ...getState().api.headers,
+    ...pathEntity.headers,
+  };
+  let body;
+  if (method == 'get' && data) {
+    realPath = realPath + '?' + stringify(data);
+  } else {
+    body = formData(data, headers['Content-Type']);
+  }
+
+  if (headers['Content-Type'] === 'multipart/form-data') {
+    delete headers['Content-Type'];
+    delete headers['content-type'];
+  }
+  //
+  const result = {
+    endpoint:
+      getState().api.protocol +
+      '://' +
+      getState().api.host +
+      getState().api.basePath +
+      realPath,
+    method: method,
+
+    headers: headers,
+    body: body,
+    types: processType(types, pathName, method),
+  };
+  return {
+    [CALL_API]: result,
+  };
+}
